8e037821460246e698d3ecc70d6be4fbb96daf92
mcgr0217 <hello@samanthamcgregor.ca>
2016-02-09 16:18:04 -0500

    feat(js): add js
    add js from in class

---
+++ js/push-menu.js
@@ -0,0 +1,19 @@
+// Needs latest version of jQuery to run
+$(function(){
+    // Shows buttons when JS runs
+ $('body').removeClass('noscript');  
+    // Sets buttons to toggle data-state
+  $('.toggle-btn, .close-btn').click(function(){
+     toggleNav(); 
+  }); 
+});
+
+function toggleNav() {
+    if ($('.site-wrapper').attr('data-state') == 'slide-closed') {
+        // display nav when closed
+     $('.site-wrapper').attr('data-state', 'slide-open');
+    } else {
+        // hide nav when open
+     $('.site-wrapper').attr('data-state', 'slide-closed');  
+    }
+}
